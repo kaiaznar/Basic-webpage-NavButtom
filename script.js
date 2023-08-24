71a2a96d908943635811e4c7af14b2a3794675c3
kaiaznar <140383811+kaiaznar@users.noreply.github.com>
2023-08-24 20:15:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function navBarShow(){
+    const navBar = document.querySelector('.container');
+    navBar.classList.toggle("container-expand");
+
+    const navBarContent = document.querySelector('nav');
+    navBarContent.classList.toggle("navBar")
+    
+    
+}
